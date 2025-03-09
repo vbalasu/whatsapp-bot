53b56870cdd7bb7dde52b33c00a88d2a9a826dfd
Vijay Balasubramaniam <vbalasu@gmail.com>
2025-03-08 17:45:42 -0800

    Deployed to AWS Lambda

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-curl -X POST http://127.0.0.1:8000/send-message \
+#ENDPOINT=http://127.0.0.1:8000/send-message
+ENDPOINT=https://pyf28n8elf.execute-api.us-east-1.amazonaws.com/api/send-message
+
+curl -X POST $ENDPOINT \
      -H "Content-Type: application/json" \
      -d '{"to": "+17324232445", "message": "Hello from WhatsApp bot!"}'
